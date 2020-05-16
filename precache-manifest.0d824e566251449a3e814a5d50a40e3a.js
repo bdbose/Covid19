fddf0bcb8975227dc66633764dad52ac56857feb
bdbose <57151177+bdbose@users.noreply.github.com>
2020-05-16 21:51:04 +0530

    Updates

---
+++ precache-manifest.0d824e566251449a3e814a5d50a40e3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "f88603929126487f495b",
+    "url": "/Covid19/static/css/main.86006d1b.chunk.css"
+  },
+  {
+    "revision": "f88603929126487f495b",
+    "url": "/Covid19/static/js/main.3387b3b7.chunk.js"
+  },
+  {
+    "revision": "964bc20fbe3b22dbf268",
+    "url": "/Covid19/static/js/runtime~main.39d2cb3d.js"
+  },
+  {
+    "revision": "c7c857b9159d18647b8e",
+    "url": "/Covid19/static/js/2.e7d5e309.chunk.js"
+  },
+  {
+    "revision": "c53e4be5aac51e4615b24dcd037f3091",
+    "url": "/Covid19/static/media/icon.c53e4be5.jpg"
+  },
+  {
+    "revision": "394257e78f266893212e95ffb8b7df38",
+    "url": "/Covid19/index.html"
+  }
+];
