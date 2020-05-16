978821efcbaa71eba84cb547a162e14166b7a227
bdbose <57151177+bdbose@users.noreply.github.com>
2020-05-16 19:34:58 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MainBody from './covid19/main.jsx';
+import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<MainBody/>,
+ReactDOM.render(
+  <React.StrictMode>
+    <MainBody/>
+  </React.StrictMode>,
   document.getElementById('root')
 );
 
+serviceWorker.unregister();
